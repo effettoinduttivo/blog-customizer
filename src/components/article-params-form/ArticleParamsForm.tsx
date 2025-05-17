505795eec555f6756bb64fb38b7b42deca5f488f
Yulya Kazitka <effettoinduttivo@gmail.com>
2025-05-17 15:32:44 +0300

    add functionality

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,145 @@
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type FormProps = {
+	setArticleState: (state: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ setArticleState }: FormProps) => {
+	const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+	useOutsideClickClose({
+		isOpen: isSidebarOpen,
+		onChange: setSidebarOpen,
+		rootRef: sidebarRef,
+	});
+
+	// useEffect(() => {
+	// 	const handleOverlayClick = (event: MouseEvent) => {
+	// 		if (
+	// 			sidebarRef.current &&
+	// 			!sidebarRef.current.contains(event.target as Node)
+	// 		) {
+	// 			setSidebarOpen(false);
+	// 		}
+	// 	};
+
+	// 	if (isSidebarOpen) {
+	// 		document.addEventListener('mousedown', handleOverlayClick);
+	// 	}
+
+	// 	return () => {
+	// 		document.removeEventListener('mousedown', handleOverlayClick);
+	// 	};
+	// }, [isSidebarOpen]);
+
+	const handleOptionChange = <K extends keyof ArticleStateType>(
+		field: K,
+		value: ArticleStateType[K]
+	) => {
+		setFormState((prevState) => ({
+			...prevState,
+			[field]: value,
+		}));
+	};
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setArticleState(formState);
+		setSidebarOpen(false);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		setArticleState(defaultArticleState);
+		setSidebarOpen(false);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isSidebarOpen}
+				onClick={() => {
+					setSidebarOpen((prev) => !prev);
+				}}
+			/>
+			<aside
+				ref={sidebarRef}
+				className={clsx(styles.container, {
+					[styles.container_open]: isSidebarOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(value) => {
+							handleOptionChange('fontFamilyOption', value);
+						}}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={(value) => {
+							handleOptionChange('fontSizeOption', value);
+						}}
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(value) => {
+							handleOptionChange('fontColor', value);
+						}}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(value) => {
+							handleOptionChange('backgroundColor', value);
+						}}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(value) => {
+							handleOptionChange('contentWidth', value);
+						}}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
